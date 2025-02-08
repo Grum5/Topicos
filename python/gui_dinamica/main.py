92d8f63debf0f302ae5e7c4f31efb2a08ef7b3fc
Grum5 <al22760591@ite.edu.mx>
2025-02-07 22:31:11 -0800

    Making comments on the files

--- python/gui_dinamica/main.py
+++ python/gui_dinamica/main.py
@@ -1,6 +1,7 @@
-
-import tkinter as tk
-from myModule.dinamic_control import Dinamic_Control
+import tkinter as tk                    # Importando tkinter como tk
+from myModule.dinamic_control import (  # Importando las clases necesarias de myModule
+    Dinamic_Control
+)
 
 class App:
     ''' Clase App, ejecuta las ventanas '''
@@ -8,17 +9,17 @@ class App:
         ''' Contructor '''
 
         # Inicializando la ventana principal
-        self.main_window = tk.Tk()
-        self.main_window.title('First app')
-        self.main_window.geometry('600x400')
-        self.main_window.resizable(False, False)
+        self.main_window = tk.Tk()                  # Instancia de la ventana raiz (root/master)
+        self.main_window.title('Gui dinamica')      # Definiendo el titulo de la ventana
+        self.main_window.geometry('600x400')        # Definiendo tamaño de la ventana
+        self.main_window.resizable(False, False)    # Definiendo propiedades de la ventana para evitar que cambie el tamaño
         
-        # Crear instancia que administre controles
+        # Crear instancia que administre controles dinamicos desde myModule
         self.control_manager = Dinamic_Control(self.main_window)
 
 # -------------------------------------------------------------------
 
 if __name__ == '__main__':
 
-    app = App()
-    app.main_window.mainloop()
+    app = App()                 # Creando objeto de la clase App
+    app.main_window.mainloop()  # Metodo de loop de tkinter
--- python/gui_dinamica/myModule/dinamic_control.py
+++ python/gui_dinamica/myModule/dinamic_control.py
@@ -6,11 +6,15 @@ class Dinamic_Control:
     ''' Clase encargada de controlar el ingreso de controles '''
 
     def __init__(self, window):
+        ''' Constructor '''
 
         # Instacia window
         self.window = window
+        
+        # Definir la cantidad maxima de controles dinamicos (Yo escogi 5)
+        self.max_dinamic_controls = 5
 
-        # Main Button | Genera mas controles con Entry y Buttons
+        # Main Button | Genera mas controles con en un Frame que tienen Entries y Buttons
         self.main_btn = tk.Button(
                 self.window,
                 text='Agregar nuevo control',
@@ -27,8 +31,8 @@ class Dinamic_Control:
     def add_new_control(self):
         ''' Funcion encargada de agregar un nuevo control con un Entry y un Button '''
 
-        # Evaluar se llego al limite de controles preestablecido
-        if self.index_ctrl_dict >= 5: return
+        # Evaluar si se llego al limite de controles preestablecido y abandonar la funcion en caso de ser verdadero
+        if self.index_ctrl_dict >= self.max_dinamic_controls: return
 
         # Incrementar el indice
         self.index_ctrl_dict += 1
@@ -42,16 +46,16 @@ class Dinamic_Control:
 
         # Crear variable para contener una tupla y evaluar las entradas
         evaluation = (
-            self.window.register(self.eval_entry),  # Se registran
-            '%S',
-            self.index_ctrl_dict
+            self.window.register(self.eval_entry),  # register() Actua como un callback, se ocupa para que funcione la evaluacion de los entry
+            '%S',                                   # Se evalua cada tecla ingresada
+            self.index_ctrl_dict                    # Se manda el index del button
         )
 
         # Generar el Entry en el contenedor
         entry = tk.Entry(
             container,
-            validate='key',      # Validar el tipo de accion, en este caso un Key
-            validatecommand=evaluation
+            validate='key',                         # Validar el tipo de accion, en este caso un Key
+            validatecommand=evaluation              
         )
         entry.pack(side=tk.LEFT, padx=5)
 
@@ -59,7 +63,10 @@ class Dinamic_Control:
         button = tk.Button(
                 container,
                 text=f"Boton {self.index_ctrl_dict}",
+
+                # Se manda una funcion anonima a command para poder mandar el index del boton
                 command=lambda index=self.index_ctrl_dict: self.show_message(index)
+
         )
         button.pack(side=tk.LEFT)
 
@@ -67,24 +74,33 @@ class Dinamic_Control:
 # -------------------------------------------------------------------
 
     def show_message(self, index):
+        ''' Metodo encargado de abrir una ventana con un mensaje '''
+        
+        # Metodo de tkinter para abrir una caja con un mensaje
         messagebox.showinfo('Boton presionado', f'Presionaste el boton {index}')
 
 # -------------------------------------------------------------------
 
     def eval_entry(self, char, index):
-        if int(index) % 2 == 0:      # Si es par se evalua si ocupa numeros
+        ''' Metodo para evaluar los eventos de keypressed ''' 
+
+        if int(index) % 2 == 0:     # Se evalua si el indice es par, si lo es se llama al metodo is_digit(char)
             return self.is_number(char)
-        else:
+        else:                       # Caso contrario se llama al metodo is_text(char)
             return self.is_text(char)
     
 
 # -------------------------------------------------------------------
 
     def is_text(self, data):
+        ''' Metodo que evalua si el dato es un caracter de texto '''
+
         return data.isalpha()
 
 # -------------------------------------------------------------------
 
     def is_number(self, data):
+        ''' Metodo que evalua si el dato es un digito '''
+
         return data.isdigit()
 
