abb49e473195370ebc6372030b121978d1de328a
Grum5 <al22760591@ite.edu.mx>
2025-03-27 22:15:42 -0700

    si

--- java/p3/p3library/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example.components","l":"CustomButton"},{"p":"org.example.components","l":"CustomTextBox"},{"p":"org.example.components","l":"InputValidator"},{"p":"org.example.components","l":"RFCValidator"}];updateSearchResults();
